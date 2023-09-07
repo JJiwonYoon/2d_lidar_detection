1c79fed1b83c0aac0293e3bea9fe07ea2c65fccf
JJiwonYoon <wldnjs0488@gmail.com>
2023-09-07 17:00:16 +0900

    pub_added

--- 2D_LiDAR_detection/2D_LiDAR_detection.py
+++ 2D_LiDAR_detection/2D_LiDAR_detection.py
@@ -17,16 +17,17 @@ class detection(Node):
         self.added_range           = int(2*((self.global_x_LiDAR)/(self.partition_cnt)))
         self.RC_group = ReentrantCallbackGroup()
         self.create_subscription(LiDAR,'/scan',self.LiDAR_callback,1, callback_group=self.RC_group)
-        # self.target_num_pub = self.create_publisher(Float32, '/LiDAR_target_num', 1, callback_group=self.RC_group)
+        self.target_num_pub = self.create_publisher(Float32, '/LiDAR_target_num', 1, callback_group=self.RC_group)
 
 
     def LiDAR_callback(self,msg):
         self.lidar_data = msg.ranges
         self.lidar_angle_increment = msg.angle_increment
         self.partition_list        = [0,0,0,0,0,0,0,0]
-        self.distance_data  = []
-        self.target_num     = 0
-        self.max_value      = 0
+        self.distance_data         = []
+        self.target_num            = 0
+        self.max_value             = 0
+        float_msg                  = Float32()
         for i in range(len(self.lidar_data)):
             angle = (self.lidar_angle_increment * i)
             x = -np.sin(angle) * self.lidar_data[i] * 1000
@@ -34,14 +35,16 @@ class detection(Node):
             
             if (abs(x) < self.global_x_LiDAR) & (y > 0) & (y < self.global_y_LiDAR): #mm
                 for i in range(self.partition_cnt):
-                    if ((-self.global_x_LiDAR)+ i*(self.added_range)) < (x) and (x) < ((-self.global_x_LiDAR)+ ((i+1)*(self.added_range))):
+                    if ((-self.global_x_LiDAR)+ i*(self.added_range)) < (x) and \
+                        (x) < ((-self.global_x_LiDAR)+ ((i+1)*(self.added_range))):
                         self.distance = (math.sqrt(x*x + y*y))
                         self.distance_data.append(self.distance)
                         self.partition_list[i] = len(self.distance_data)
                 self.target_num, self.max_value = max(enumerate(self.partition_list),key=lambda x: x[1])
                 self.target_num +=1
+        float_msg.data= float(self.target_num)
         print(self.target_num,self.max_value)
-        # self.target_num_pub.publish(self.target_num)
+        self.target_num_pub.publish(float_msg)
                 
 def main():
         rclpy.init()
