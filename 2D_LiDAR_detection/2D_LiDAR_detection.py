66ab507c3fd6de6d0c5d648a2ed0936b5dccbfd8
JJiwonYoon <wldnjs0488@gmail.com>
2023-09-07 15:35:20 +0900

    good

--- 2D_LiDAR_detection/2D_LiDAR_detection.py
+++ 2D_LiDAR_detection/2D_LiDAR_detection.py
@@ -14,11 +14,11 @@ class detection(Node):
 
         self.filter_distance       = 0 ##mm
         self.partition_cnt         = 8
-        self.global_x_LiDAR        = 200 #mm
+        self.global_x_LiDAR        = 240 #mm
         self.global_y_LiDAR        = 200
         self.distance              = 0   #mm
-        self.point_num             = 1
-        
+        self.point_num             = 10
+        self.added_range           = int(2*((self.global_x_LiDAR)/(self.partition_cnt)))
         self.RC_group = ReentrantCallbackGroup()
         self.create_subscription(LiDAR,'/scan',self.LiDAR_callback,1, callback_group=self.RC_group)
 
@@ -26,37 +26,58 @@ class detection(Node):
     def LiDAR_callback(self,msg):
         self.lidar_data = msg.ranges
         self.lidar_angle_increment = msg.angle_increment
+        self.partition_list        = [0,0,0,0,0,0,0,0]
         self.converted_data = []
         self.distance_data  = []
         self.x_filter       = 0
         self.y_filter       = 0
         self.partition_num  = 0
-
+        self.target_num     = 0
+        self.max_value      = 0
         for i in range(len(self.lidar_data)):
             angle = (self.lidar_angle_increment * i)
             x = -np.sin(angle) * self.lidar_data[i] * 1000
             y = np.cos(angle) * self.lidar_data[i] * 1000
+            
             if (abs(x) < self.global_x_LiDAR) & (y > 0) & (y < self.global_y_LiDAR): #mm
-                
-                self.distance = (math.sqrt(x*x + y*y))
-                self.distance_data.append(self.distance)
-                if len(self.distance_data) > (self.point_num):
-                    self.converted_data.append([x,y])
-                    x_values = [item[0] for item in self.converted_data]
-                    y_values = [item[1] for item in self.converted_data]
-                    self.x_filter = sum(x_values) / len(x_values)
-                    self.y_filter = sum(y_values) / len(y_values)
-                    self.filter_distance = (math.sqrt((self.x_filter*self.x_filter)+(self.y_filter*self.y_filter)))
-                    # print("x_filter:", self.x_filter,"y_filter:", self.y_filter)
-                    # print(((-self.global_x_LiDAR/2)+((self.global_x_LiDAR)/(self.partition_cnt))))
-                    # print(self.filter_distance)
-                    if ((-self.global_x_LiDAR)/2) < (self.x_filter) and (self.x_filter) < ((-self.global_x_LiDAR/2)+((self.global_x_LiDAR)/(self.partition_cnt))):
-                        if self.filter_distance > 300:
-                            self.partition_num = 1
-                            print(self.partition_num)
-                            
-                        
+                # partition 1
+                if ((-self.global_x_LiDAR)) < (x) and (x) < ((-self.global_x_LiDAR)+(self.added_range)):
+                    self.distance = (math.sqrt(x*x + y*y))
+                    self.distance_data.append(self.distance)
+                    self.partition_list[0] = len(self.distance_data)
+                if ((-self.global_x_LiDAR)+(self.added_range)) < (x) and (x) < ((-self.global_x_LiDAR)+2*(self.added_range)):
+                    self.distance = (math.sqrt(x*x + y*y))
+                    self.distance_data.append(self.distance)
+                    self.partition_list[1] = len(self.distance_data)
+                if ((-self.global_x_LiDAR)+2*(self.added_range)) < (x) and (x) < ((-self.global_x_LiDAR)+3*(self.added_range)):
+                    self.distance = (math.sqrt(x*x + y*y))
+                    self.distance_data.append(self.distance)
+                    self.partition_list[2] = len(self.distance_data)
+                if ((-self.global_x_LiDAR)+3*(self.added_range)) < (x) and (x) < ((-self.global_x_LiDAR)+4*(self.added_range)):
+                    self.distance = (math.sqrt(x*x + y*y))
+                    self.distance_data.append(self.distance)
+                    self.partition_list[3] = len(self.distance_data)
+                if ((-self.global_x_LiDAR)+4*(self.added_range)) < (x) and (x) < ((-self.global_x_LiDAR)+5*(self.added_range)):
+                    self.distance = (math.sqrt(x*x + y*y))
+                    self.distance_data.append(self.distance)
+                    self.partition_list[4] = len(self.distance_data)
+                if ((-self.global_x_LiDAR)+5*(self.added_range)) < (x) and (x) < ((-self.global_x_LiDAR)+6*(self.added_range)):
+                    self.distance = (math.sqrt(x*x + y*y))
+                    self.distance_data.append(self.distance)
+                    self.partition_list[5] = len(self.distance_data)
+                if ((-self.global_x_LiDAR)+6*(self.added_range)) < (x) and (x) < ((-self.global_x_LiDAR)+7*(self.added_range)):
+                    self.distance = (math.sqrt(x*x + y*y))
+                    self.distance_data.append(self.distance)
+                    self.partition_list[6] = len(self.distance_data)
+                if ((-self.global_x_LiDAR)+7*(self.added_range)) < (x) and (x) < ((-self.global_x_LiDAR)+8*(self.added_range)):
+                    self.distance = (math.sqrt(x*x + y*y))
+                    self.distance_data.append(self.distance)
+                    self.partition_list[7] = len(self.distance_data)
 
+                self.target_num, self.max_value = max(enumerate(self.partition_list),key=lambda x: x[1])
+                self.target_num +=1
+        print(self.target_num,self.max_value)
+                
 def main():
         rclpy.init()
         C = detection()
